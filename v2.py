87e0462772614b403c117adc77dc19e215c373f1
Eli Abidor <eabidor@gmail.com>
2024-08-14 21:27:49 -0400

    put multihead and ffwd in blocks and repeat block several times to intersperse communication and calculation

--- v2.py
+++ v2.py
@@ -111,6 +111,21 @@ class FeedForward(nn.Module):
   def forward(self, x):
     return self.net(x)
 
+class Block(nn.Module):
+  """ Transformer block: communication followed by computation """
+
+  def __init__(self, n_embd, n_head):
+    # n_embd: embedding decision, n_head: the number of heads we'd like
+    super().__init__()
+    head_size = n_embd // n_head
+    self.sa = MultiHeadAttention(n_head, head_size)
+    self.ffwd = FeedForward(n_embd)
+
+  def forward(self, x):
+    x = self.sa(x)
+    x = self.ffwd(x)
+    return x
+
 # super simple bigram language model
 class BigramLanguageModel(nn.Module):
 
@@ -119,8 +134,11 @@ class BigramLanguageModel(nn.Module):
     # each token directly reads off the logits for the next token from a lookup table
     self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
     self.position_embedding_table = nn.Embedding(block_size, n_embd)
-    self.sa_heads = MultiHeadAttention(4, n_embd//4) # i.e. 4 heads of 8-dimensional self-attention
-    self.ffwd = FeedForward(n_embd)
+    self.blocks = nn.Sequential(
+      Block(n_embd, n_head=4),
+      Block(n_embd, n_head=4),
+      Block(n_embd, n_head=4),
+    )
     self.lm_head = nn.Linear(n_embd, vocab_size)
 
   def forward(self, idx, targets=None):
